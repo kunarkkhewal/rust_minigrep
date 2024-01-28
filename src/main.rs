0f2040cfcb7335719d369d0821d3ed3253431b92
kunarkkhewal <kunark.khewal88@gmail.com>
2024-01-28 17:35:58 +0530

    saving command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    // dbg!(args);
+
+    let query = &args[1];
+    let filepath = &args[2];
+
+    println!("Searching {}", query);
+    println!("In file {}", filepath);
 }
