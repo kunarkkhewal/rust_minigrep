228739889627e4aa9416c647c7e02cdb617bfe62
kunarkkhewal <kunark.khewal88@gmail.com>
2024-01-28 22:26:26 +0530

    code split into main and lib file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use std::{env, fs, process, error::Error};
+use std::{env, process};
+
+use rust_minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,34 +13,8 @@ fn main() {
     println!("Searching {}", config.query);
     println!("In file {}", config.filepath);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = rust_minigrep::run(config) {
         println!("Application error, {e}");
         process::exit(1);
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filepath)?;
-        // .expect("Should have been able to read file");
-    
-    println!("With Text:\n{contents}");
-
-    Ok(())
-}
-
-#[derive(Debug)]
-struct Config {
-    query: String,
-    filepath: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-        let query = args[1].clone();
-        let filepath = args[2].clone();
-        Ok(Config {query, filepath})
     }
 }
