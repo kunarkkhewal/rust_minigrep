68aabed7bdd8f4468590f3a40c64a3225baa7860
kunarkkhewal <kunark.khewal88@gmail.com>
2024-01-28 20:38:21 +0530

    refactored code to handle arguments

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,27 @@ use std::{env, fs};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (query, filepath) = parse_config(&args);
+    let config = Config::new(&args); //parse_config(&args);
 
-    
+    println!("Searching {}", config.query);
+    println!("In file {}", config.filepath);
 
-    println!("Searching {}", query);
-    println!("In file {}", filepath);
-
-    let contents = fs::read_to_string(filepath)
+    let contents = fs::read_to_string(config.filepath)
         .expect("Should have been able to read file");
 
     println!("With Text:\n{contents}")
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filepath = &args[2];
+#[derive(Debug)]
+struct Config {
+    query: String,
+    filepath: String,
+}
 
-    (query, filepath)
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filepath = args[2].clone();
+        Config {query, filepath}
+    }
 }
