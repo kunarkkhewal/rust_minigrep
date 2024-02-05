28fefbd551e615d6a1525b92ab82004b9a583b6b
kunarkkhewal <kunark.khewal88@gmail.com>
2024-02-05 21:49:46 +0530

    removed println and added test case for search function, with basic search fn implemented

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,6 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filepath)?;
         // .expect("Should have been able to read file");
     
-    println!("With Text:\n{contents}");
-
     Ok(())
 }
 
@@ -23,5 +21,25 @@ impl Config {
         let query = args[1].clone();
         let filepath = args[2].clone();
         Ok(Config {query, filepath})
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    vec![]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+        Rust:
+        safe, fast, productive.
+        Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents))
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,11 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
-    println!("Searching {}", config.query);
-    println!("In file {}", config.filepath);
 
     if let Err(e) = rust_minigrep::run(config) {
-        println!("Application error, {e}");
         process::exit(1);
     }
 }
