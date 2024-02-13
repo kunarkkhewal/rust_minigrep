9a634ee371df7e803476ab0191e11de6589ebbeb
kunarkkhewal <kunark.khewal88@gmail.com>
2024-02-13 19:54:02 +0530

    removed commented code

--- src/lib.rs
+++ src/lib.rs
@@ -25,13 +25,6 @@ pub struct Config {
 
 impl Config {
     pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
-        // if args.len() < 3 {
-        //     return Err("Not enough arguments");
-        // }
-        
-        // let query = args[1].clone();
-        // let filepath = args[2].clone();
-        
         args.next();
 
         let query = match args.next() {
@@ -50,15 +43,6 @@ impl Config {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    // let mut results = Vec::new();
-
-    // for line in contents.lines() {
-    //     if line.contains(query) {
-    //         results.push(line);
-    //     }
-    // }
-
-    // results
     contents
         .lines()
         .filter(|line| line.contains(query))
@@ -67,15 +51,6 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str,) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    // let mut results = Vec::new();
-
-    // for line in contents.lines() {
-    //     if line.to_lowercase().contains(&query) {
-    //         results.push(line);
-    //     }
-    // }
-
-    // results
     contents
         .lines()
         .filter(|line| line.to_lowercase().contains(&query))
